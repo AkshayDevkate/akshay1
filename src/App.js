c5a6bcf2694978f700e2e3c111c4b15b98ee5155
Akshay Devkate <devkatte.akshay98@gmail.com>
2022-01-09 22:39:52 +0100

    Added pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as  Router,Route, Link,Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router>
+    <div className="List">
+  <ul>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="About">About</Link></li>
+    <li><Link to="Blogs">Blogs</Link></li>
+  </ul>
+  </div>
+ 
+        <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="/About" element={<About />}/>
+         <Route exact path="/Blogs" element={<Blogs />}/>
+        </Routes>
+
+
+
+
+         </Router>
+
+
+</div>
+
   );
 }
 
