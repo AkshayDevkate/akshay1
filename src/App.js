a5673cead1450ff8f5c8b9f204e9f735b1935f96
Akshay Dattatray Devkate <42045980+AkshayDevkate@users.noreply.github.com>
2022-01-11 18:19:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter as  Router,Route, Link,Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
 import './App.css';
-import Header from './Header';
-import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-    <Navbar />
-      <header className="App-header">
-       <h2> Akshay Devkate 💻 🐐</h2>
-       <h3>Travel, Tech, Business, Analytics</h3>
-          <Header />
-       </header>
-  
-     </div>
-  );
+     <Router>
+    <div className="List">
+  <ul>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="About">About</Link></li>
+    <li><Link to="Blogs">Blogs</Link></li>
+  </ul>
+  </div>
+    <h2> Akshay Devkate 💻 🐐</h2>
+        <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="/About" element={<About />}/>
+         <Route exact path="/Blogs" element={<Blogs />}/>
+        </Routes>
+   </Router>
+</div>
+);
 }
 
 export default App;
